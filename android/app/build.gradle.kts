20e0a0b0b11337cdb29c36ca241cf66d38986d73
ahlanhabib <ahlanhabib@example.com>
2026-04-27 09:10:14 +0700

    Update package name to ma.educinta.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "ma.educinta.educinta_app"
+    namespace = "ma.educinta.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "25.2.9519653"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "ma.educinta.educinta_app"
+        applicationId = "ma.educinta.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
